5cf5883ac89c98d01c7f24c875cb8b78a9148ae1
Sidnei Lechado <51683568+sidlechado@users.noreply.github.com>
2020-08-02 16:17:38 -0300

    Update mussun.py

--- mussun.py
+++ mussun.py
@@ -1,31 +1,49 @@
-# Mussum recently bought a pack of Cacildis, which each can has an specific
-# height. The cans in the pack are stored sequentially. The height of each can is
-# a number in a permutation from 1 to n. A permutation of 1 to n is a sequence
-# of n numbers in which each number from 1 to n occurs exactly once.
-# In this pack of Cacildis, Mussum wants to now if there is a can that is taller
-# than any can on its left and right. That is: given a permutation p1, p2, ..., pn, if
-# there are pi
-# , pj , pk such that i < j < k and pi < pj and pj > pk.
+# print("Welcom to the Mussun Cacildis!")
+# print("The instructions are as follows:")
+# print("1. The number of games can be between 1 and 200.")
+# print("2. Each pack can have between 3 and 100 Cans.")
 
-print("Welcom to the Mussun Cacildis!")
-print("The instructions are as follows:")
-print("1. The number of games can be between 1 and 200.")
-print("2. Each pack can have between 3 and 100 Cans.")
+# while True:
+#   nr_test_cases = int(input("Enter the number of games: "))
+#   if nr_test_cases > 0 and nr_test_cases < 201:
+#     break
+#   else:
+#     print('Invalid number of tests. It should be between 1 and 200.\n')
 
-while True:
-  nr_test_cases = int(input("Enter the number of games: "))
-  if nr_test_cases > 0 and nr_test_cases < 201:
-    break
-  else:
-    print('Invalid number of tests. It should be between 1 and 200.\n')
+def verify_right(right_part_of_list, height_being_verified, current_index):
+  i = 0
+  controller = [False, []]
+  while i < len(right_part_of_list):
+    if height_being_verified > right_part_of_list[i]:
+      controller[0] = True
+      controller[1] = controller[1] + (i + current_index)
+    i += 1
+  return controller
 
+## VERIFY AND DRAW
+def verify_left(left_part_of_list, height_being_verified, current_index):
+  i = 0
+  controller = [False, []]
+  while i < len(left_part_of_list):
+    if height_being_verified > left_part_of_list[i]:
+      controller[0] = True
+      controller[1] = controller[1] + (i + current_index)
+    i += 1
+  return controller
 
-heights = [int(heights) for heights in input().split()] 
-
-i = 5
-while i > 0:
-  print(i)
-  i = i - 1
+lenght = int(input('lenght of permutation'))
+heights = [int(heights) for heights in input('heights').split()] 
+empty_solution_list = []
+i = 0
+while i < lenght:
+  left_verification= [] #Will return on index 0 a true or false, and on index 1, all the indexes
+  right_verification = []
+  #Quebrar a verificacao em direita e esquerda, se é o primeiro ou ultimo, n verifica
+  if i != 0 or i != (lenght - 1):
+    left_verification= verify_left(heights[0:i-1], heights[i], i)
+    right_verification = verify_right(heights[i+1:lenght-1], heights[i], i)
+  print(heights[i])
+  i += 1
   
 
 # for can in heights:
